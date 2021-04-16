24f3268fee495d446b822f7b0ca54f2cd566a847
Rubyroy12 <ibrakipz7@gmail.com>
2021-04-16 15:37:41 +0300

    add jquery effects to whatwedo section

--- js/app.js
+++ js/app.js
@@ -1,4 +1,35 @@
 $(document).ready(function(){
     // alert("page under development process");
 
+    //design onclick toggle
+    $(".d-image").click(function(){
+        $(".design").show();
+        $(".d-image").hide();
+    });
+
+    $(".design").click(function(){
+        $(".d-image").show();
+        $(".design").hide();
+    });
+    $(".dev-image").click(function(){
+        $(".development").show();
+        $(".dev-image").hide();
+    })
+    $(".development").click(function(){
+        $(".dev-image").show();
+        $(".development").hide();
+
+    })
+    $(".p-image").click(function(){
+        $(".product").show();
+        $(".p-image").hide();
+    });
+    $(".product").click(function(){
+        $(".product").hide();
+        $(".p-image").show();
+    });
+    
+
+
+
 })
