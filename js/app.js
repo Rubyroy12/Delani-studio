8d5cda3d2ba09230865f834379fcce9b07fa1163
Rubyroy12 <ibrakipz7@gmail.com>
2021-04-18 15:43:27 +0300

    add js function

--- js/app.js
+++ js/app.js
@@ -1,34 +1,51 @@
-$(document).ready(function(){
+$(document).ready(function () {
     // alert("page under development process");
 
     //design onclick toggle
-    $(".d-image").click(function(){
+    $(".d-image").click(function () {
         $(".design").show();
         $(".d-image").hide();
     });
 
-    $(".design").click(function(){
+    $(".design").click(function () {
         $(".d-image").show();
         $(".design").hide();
     });
-    $(".dev-image").click(function(){
+    $(".dev-image").click(function () {
         $(".development").show();
         $(".dev-image").hide();
     })
-    $(".development").click(function(){
+    $(".development").click(function () {
         $(".dev-image").show();
         $(".development").hide();
 
     })
-    $(".p-image").click(function(){
+    $(".p-image").click(function () {
         $(".product").show();
         $(".p-image").hide();
     });
-    $(".product").click(function(){
+    $(".product").click(function () {
         $(".product").hide();
         $(".p-image").show();
     });
-    
+    $("document").ready(function () {
+        $("#form").submit(function(event) {
+            event.preventDefault();
+            var uname = $("#name").val();
+            var uemail = $("#email").val();
+            var message = $("#message").val();
+
+            if (message == " ") {
+                alert(uname + ", PLease give us feedback");
+
+            } else {
+                alert(uemail + "We have received your email.");
+            }
+
+
+        })
+    })
+
 
 
 
